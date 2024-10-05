ca98a78c6b5c66039a65f2dfce84080e2bfcd7e8
Hisham <159368595+IAmMyGuy21th@users.noreply.github.com>
2024-10-04 19:33:45 -0700

    Update pxt.json, main.blocks, util.ts

--- util.ts
+++ util.ts
@@ -17,4 +17,17 @@ namespace util {
     export function runInParallel(handler: () => void) {
         control.runInParallel(handler)
     }
+
+    /**
+     * TODO: puts a sprite specified layers behind or in front of othersprite
+     * @param sprite sprite #1
+     * @param othersprite sprite #2
+     * @param layer how many layers in front or behind
+     */
+    //% block="Put $sprite $layer layers in front of $othersprite"
+    export function putSpriteLayerOfOthersprite(sprite: Sprite, othersprite: Sprite, layer: number) {
+        sprite.z = othersprite.z + layer
+    }
+
+    
 }
