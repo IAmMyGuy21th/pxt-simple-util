79364765fa51f74169138e7df011dbb5fc80ca94
Hisham <159368595+IAmMyGuy21th@users.noreply.github.com>
2024-10-06 10:30:36 -0700

    Update pxt.json, main.blocks, util.ts

--- util.ts
+++ util.ts
@@ -28,4 +28,23 @@ namespace util {
     export function putSpriteLayerOfOthersprite(sprite: Sprite, othersprite: Sprite, layer: number) {
         sprite.z = othersprite.z + layer
     }
+
+
+    //% block="All instances of tile: $tile return first instance?: $first"
+    //% tile.shadow=tileset_tile_picker
+    //% tile.decompileIndirectFixedInstances=true
+    export function instancesOfTile(tile: Image, first: boolean) {
+        let positions = []
+        let pos = vectors.vectorZero().clone()
+        for (let x = 0; x < tiles.tilemapColumns(); x++) {
+            for (let y = 0; y < tiles.tilemapRows(); y++) {
+                pos.x = x
+                pos.y = y
+                if (tiles.tileAtLocationEquals(vectors.vectorAsTilepos(pos), tile)) {
+                    positions.push(vectors.vectorAsTilepos(pos))
+                }
+            }
+        }
+        return (first ? positions[0] : positions)
+    }
 }
